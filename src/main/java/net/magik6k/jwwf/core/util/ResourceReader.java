7b37e95f0e7c6c95cb708dbd81c191818c9df869
Łukasz Magiera <magik6k@gmail.com>
2014-10-22 22:44:46 +0200

    Fixed fix..

--- src/main/java/net/magik6k/jwwf/core/util/ResourceReader.java
+++ src/main/java/net/magik6k/jwwf/core/util/ResourceReader.java
@@ -1,26 +1,19 @@
 package net.magik6k.jwwf.core.util;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.net.URL;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 
 public class ResourceReader {
 	public static final String fileBase = "net/magik6k/jwwf/assets/";
 	public static final ResourceReader instance = new ResourceReader();
 	
 	public String readFile(String fileName){
-		Path uri = null;
+		URL url = Resources.getResource(fileBase + fileName);
 		try {
-			uri = Paths.get(this.getClass().getResource(fileBase + fileName).toURI());
-		} catch (URISyntaxException e1) {
-			e1.printStackTrace();
-		}
-		
-		try {			
-			return new String(Files.readAllBytes(uri), StandardCharsets.UTF_8);
+			return Resources.toString(url, Charsets.UTF_8);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
