62a4d10bdc9e644cc1574afbd2f30754c296dbbf
Łukasz Magiera <magik6k@gmail.com>
2014-11-28 20:01:15 +0100

    Example can be given custom API url

--- src/main/java/net/magik6k/jwwf/example/Main.java
+++ src/main/java/net/magik6k/jwwf/example/Main.java
@@ -11,8 +11,17 @@ public class Main {
 			System.out.println( "No port specified. Defaulting to 8888" );
 			port = 8888;
 		}
+		JwwfServer server = new JwwfServer(port);
+		try {
+			String addr = args[1];
+			if(addr != null){
+				server.setApiUrl(addr);
+			}
+		} catch( Exception e ) {
+			System.out.println( "No custom API address given, using automatic mode." );
+		}
 		JwwfServer.debugOutput(true);
-		JwwfServer server = new JwwfServer(port).bindWebapp(ExampleClient.class).start();
+		server.bindWebapp(ExampleClient.class).start();
 		server.start();
 	}
 }
