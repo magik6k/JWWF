6faccce4adf840b51b3c22858498ebde64bee8db
Łukasz Magiera <magik6k@gmail.com>
2015-03-19 22:50:40 +0100

    NamedPanel should have one element
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- src/main/java/net/magik6k/jwwf/widgets/basic/panel/NamedPanel.java
+++ src/main/java/net/magik6k/jwwf/widgets/basic/panel/NamedPanel.java
@@ -7,133 +7,27 @@ import net.magik6k.jwwf.util.Json;
 /**
  * Created by magik6k on 3/19/15.
  */
-public class NamedPanel extends Panel {
+public class NamedPanel extends Widget {
 	private String name = "Unnamed";
 	private Type type = Type.DEFAULT;
-
-	/**
-	 * Creates new panel with default 6 width
-	 * @param elements Number of elements
-	 */
-	public NamedPanel(int elements) {
-		super(elements);
-	}
-
-	/**
-	 * Creates new panel with default 4 width
-	 * @param width Panel width(0 to 12)
-	 * @param elements Number of elements
-	 */
-	public NamedPanel(int width, int elements) {
-		super(width, elements);
-	}
-
-	/**
-	 * @param elements Number of elements
-	 * @param widgets Default widgets
-	 */
-	public NamedPanel(int elements, Widget... widgets) {
-		super(elements, widgets);
-	}
+	protected Widget content;
 
 	/**
 	 * Creates new panel with default 6 width and one widget
 	 * @param widget Widget to store in this panel
 	 */
 	public NamedPanel(Widget widget) {
-		this(1, widget);
+		attach(widget);
+		content = widget;
 	}
-
-	/**
-	 * @param width Panel width(0 to 12)
-	 * @param elements Number of elements
-	 * @param elements Default widgets
-	 */
-	public NamedPanel(int width, int elements, Widget... widgets) {
-		super(width, elements, widgets);
-	}
-
-	/**
-	 * Creates new panel with default 6 width
-	 * @param elements Number of elements
-	 * @param type Type of the panel
-	 */
-	public NamedPanel(int elements, Type type) {
-		super(elements);
-		this.type = type;
-	}
-
-	/**
-	 * Creates new panel with default 4 width
-	 * @param width Panel width(0 to 12)
-	 * @param elements Number of elements
-	 * @param type Type of the panel
-	 */
-	public NamedPanel(int width, int elements, Type type) {
-		super(width, elements);
-		this.type = type;
-	}
-
-	/**
-	 * @param elements Number of elements
-	 * @param type Type of the panel
-	 * @param widgets Default widgets
-	 */
-	public NamedPanel(int elements, Type type, Widget... widgets) {
-		super(elements, widgets);
-		this.type = type;
-	}
-
 	/**
 	 * Creates new panel with default 6 width and one widget
 	 * @param widget Widget to store in this panel
 	 * @param type Type of the panel
 	 */
 	public NamedPanel(Widget widget, Type type) {
-		this(1, widget);
+		this(widget);
 		this.type = type;
-	}
-
-	/**
-	 * @param width Panel width(0 to 12)
-	 * @param elements Number of elements
-	 * @param elements Default widgets
-	 * @param type Type of the panel
-	 */
-	public NamedPanel(int width, int elements, Type type, Widget... widgets) {
-		super(width, elements, widgets);
-		this.type = type;
-	}
-
-	/**
-	 * Creates new panel with default 6 width
-	 * @param elements Number of elements
-	 * @param name Initial name / label
-	 */
-	public NamedPanel(int elements, String name) {
-		super(elements);
-		this.name = name;
-	}
-
-	/**
-	 * Creates new panel with default 4 width
-	 * @param width Panel width(0 to 12)
-	 * @param elements Number of elements
-	 * @param name Initial name / label
-	 */
-	public NamedPanel(int width, int elements, String name) {
-		super(width, elements);
-		this.name = name;
-	}
-
-	/**
-	 * @param elements Number of elements
-	 * @param name Initial name / label
-	 * @param widgets Default widgets
-	 */
-	public NamedPanel(int elements, String name, Widget... widgets) {
-		super(elements, widgets);
-		this.name = name;
 	}
 
 	/**
@@ -142,56 +36,8 @@ public class NamedPanel extends Panel {
 	 * @param name Initial name / label
 	 */
 	public NamedPanel(Widget widget, String name) {
-		this(1, widget);
+		this(widget);
 		this.name = name;
-	}
-
-	/**
-	 * @param width Panel width(0 to 12)
-	 * @param elements Number of elements
-	 * @param name Initial name / label
-	 * @param elements Default widgets
-	 */
-	public NamedPanel(int width, int elements, String name, Widget... widgets) {
-		super(width, elements, widgets);
-		this.name = name;
-	}
-
-	/**
-	 * Creates new panel with default 6 width
-	 * @param elements Number of elements
-	 * @param name Initial name / label
-	 * @param type Type of the panel
-	 */
-	public NamedPanel(int elements, String name, Type type) {
-		super(elements);
-		this.name = name;
-		this.type = type;
-	}
-
-	/**
-	 * Creates new panel with default 4 width
-	 * @param width Panel width(0 to 12)
-	 * @param elements Number of elements
-	 * @param name Initial name / label
-	 * @param type Type of the panel
-	 */
-	public NamedPanel(int width, int elements, String name, Type type) {
-		super(width, elements);
-		this.name = name;
-		this.type = type;
-	}
-
-	/**
-	 * @param elements Number of elements
-	 * @param name Initial name / label
-	 * @param type Type of the panel
-	 * @param widgets Default widgets
-	 */
-	public NamedPanel(int elements, String name, Type type, Widget... widgets) {
-		super(elements, widgets);
-		this.name = name;
-		this.type = type;
 	}
 
 	/**
@@ -201,24 +47,10 @@ public class NamedPanel extends Panel {
 	 * @param type Type of the panel
 	 */
 	public NamedPanel(Widget widget, String name, Type type) {
-		this(1, widget);
+		this(widget);
 		this.name = name;
 		this.type = type;
 	}
-
-	/**
-	 * @param width Panel width(0 to 12)
-	 * @param elements Number of elements
-	 * @param name Initial name / label
-	 * @param type Type of the panel
-	 * @param elements Default widgets
-	 */
-	public NamedPanel(int width, int elements, String name, Type type, Widget... widgets) {
-		super(width, elements, widgets);
-		this.name = name;
-		this.type = type;
-	}
-
 	/**
 	 * @param name Name to set
 	 * @return This widget
@@ -248,15 +80,9 @@ public class NamedPanel extends Panel {
 
 	@Override
 	public String getData() {
-		String data = "";
-		for (int i = 0; i < content.length; ++i) {
-			if (i > 0) data += ",";
-			data += "\"" + String.valueOf(content[i] != null ? content[i].getID() : -1) + "\"";
-		}
-	return new StringBuilder().append("{\"type\":\"").append(type.name)
-			.append("\",\"width\":").append(width)
-			.append(",\"name\":").append(Json.escapeString(name))
-			.append(",\"content\":[").append(data)
-			.append("]}").toString();
+		return new StringBuilder().append("{\"type\":\"").append(type.name)
+			.append("\",\"name\":").append(Json.escapeString(name))
+			.append(",\"content\":").append(String.valueOf(content != null ? content.getID() : -1))
+			.append("}").toString();
 	}
 }
--- src/main/resources/net/magik6k/jwwf/assets/webclient/widgets/panel/NamedPanel.js
+++ src/main/resources/net/magik6k/jwwf/assets/webclient/widgets/panel/NamedPanel.js
@@ -1,25 +1,24 @@
 {
 	create: function(data) {
-		var elements = [];
+		var element;
 		var body = $("<div>").addClass("panel-body")
 
-		for(var i = 0; i < data.content.length; ++i) {
-			if(data.content[i] < 0) {
-				elements[i] = {};
-				elements[i].elem = $("<div>")
+		if(data.content < 0) {
+			element = {};
+			element.elem = $("<div>")
 
-				elements[i].id = -1;
-			} else {
-				elements[i] = {};
-				elements[i].elem = widgetStorage[data.content[i]].element
-				elements[i].id = data.content[i];
-			}
-			body.append(elements[i].elem)
+			element.id = -1;
+		} else {
+			element = {};
+			element.elem = widgetStorage[data.content].element
+			element.id = data.content;
 		}
+		body.html(element.elem)
+
 		var heading = $("<div>").addClass("panel-heading").html(data.name)
 		var elem = $("<div>").addClass("panel").addClass("panel-"+data.type)
 			.append(heading).append(body)
-		return {element: elem, data: {body: body, elements: elements}};
+		return {element: elem, data: {body: body, element: element}};
 	},
 	update: function(widget, data) {
 		widget.element.removeClass(function(i, j) {
@@ -27,21 +26,15 @@
 		});
 		widget.element.addClass("panel-"+data.type)
 
-		for(var i = 0; i < data.content.length; ++i) {
-			if(data.content[i] < 0)continue;//TODO: Is this ok?
-			if(data.content[i] == widget.data.elements[i].id)continue;
+		if(data.content < 0) return;//TODO: Is this ok?
+		if(data.content == widget.data.element.id) return;
 
-			if(widget.data.elements[i].id > 0) {
-				widgetStorage[widget.data.elements[i].id].element = $(widgetStorage[widget.data.elements[i].id].element).detach();
-			}
-
-			if(i === 0) {
-				$(widget.data.body).prepend(widgetStorage[data.content[i]].element);
-			} else {
-				$(widget.data.body).children().eq(i - 1).after(widgetStorage[data.content[i]].element);
-			}
-			//$(widget.data.elements[i].elem).html(widgetStorage[data.content[i]].element);
-			widget.data.elements[i].id = data.content[i];
+		if(widget.data.element.id > 0) {
+			widgetStorage[widget.data.element.id].element = $(widgetStorage[widget.data.element.id].element).detach();
 		}
+
+		$(widget.data.body).html(widgetStorage[data.content].element);
+		widget.data.elements[i].id = data.content[i];
+
 	}
 };//Row end
